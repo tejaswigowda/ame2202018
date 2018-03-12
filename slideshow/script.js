39fcde175af642022df1e5af2a496296cd09af41
tejaswigowda <tejaswi@foxyninjastudios.com>
2018-03-12 12:01:16 -0700

    in class

--- slideshow/script.js
+++ slideshow/script.js
@@ -1,13 +1,25 @@
-var allImages = [
+var allImages = [];
+
+var currentSlide = 1;
+var startEmbed = function()
+{
+  allImages = location.hash.replace("#","").split(",");
+  startAux();
+}
+
+var start = function()
+{
+  allImages = [
   'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Pied-winged_swallow_%28Hirundo_leucosoma%29.jpg/1280px-Pied-winged_swallow_%28Hirundo_leucosoma%29.jpg',
   'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Rufous-tailed_flycatcher_%28Myiarchus_validus%29.JPG/1024px-Rufous-tailed_flycatcher_%28Myiarchus_validus%29.JPG',
   'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Bare-faced_curassow_%28Crax_fasciolata%29_female_head.JPG/1024px-Bare-faced_curassow_%28Crax_fasciolata%29_female_head.JPG',
   'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Savanna_hawk_%28Buteogallus_meridionalis%29.JPG/800px-Savanna_hawk_%28Buteogallus_meridionalis%29.JPG'
 ];
+  startAux();
+}
 
-var currentSlide = 1;
 
-var start = function()
+function startAux()
 {
   var markup = "";
   for(var i = 0; i < allImages.length; i++){
