f801b1b8e86b7882411d2c644fbd4c78aa01781a
tejaswigowda <tejaswi@foxyninjastudios.com>
2018-03-19 11:47:58 -0700

    in class

--- basicServers/evalServer.js
+++ basicServers/evalServer.js
@@ -1,11 +1,12 @@
 var http = require("http");
 
 var callback = function (req, res) { // req -> request object; res -> response object
+  var eq = req.url.replace("/","");;
+  if(eq.indexOf("favicon") >=0 ) return;
+  var r = eq + " = " + eval(eq) + "\n";
   res.writeHead(200, {'Content-Type': 'text/plain'}); // send response header
-	var st = "hello world"
-  res.end(st); // send response body
+  res.end(r); // send response body
 }
 
 var server = http.createServer(callback) // create an http server
-server.listen(4321); // make server listen to port 1234
-//Server running at http://127.0.0.1:1234
+server.listen(1234); // make server listen to port 1234
