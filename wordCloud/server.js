44df8ff03c8a0fbfd1a9c6c7a0361b2afb4fd81e
tejaswigowda <tejaswi@foxyninjastudios.com>
2018-03-21 11:39:10 -0700

    AC

---
+++ wordCloud/server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 1234;
+
+app.get("/", function (req, res) {
+    res.redirect("index.html")
+});
+
+app.get("/eval", function (req, res) { 
+  var eq = req.query.eq;
+  if(eq.indexOf("favicon") >=0 ) return;
+  var r = eq + " = " + eval(eq) + "\n";
+  res.writeHead(200, {'Content-Type': 'text/plain'}); 
+  res.end(r); // send response body
+});
+      
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
